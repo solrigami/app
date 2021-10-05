85e93d651faeaba826d037386738003507dc6be3
Welison Almeida <welison.almeida.923@gmail.com>
2021-10-04 22:46:40 -0300

    Configure material UI to project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import SendIcon from '@mui/icons-material/Send';
+import Button from "@material-ui/core/Button";
+
+const theme = createTheme({
+  palette: {
+    primary: { light: "#FFB703", main: "#FB8500", contrastText: "#FFFFFF" },
+    secondary: { main: "#58E997" },
+    success: { main: "#4BB543" },
+    error: { main: "#E10050" },
+  },
+});
 
 function App() {
-  return <div>Let's go!</div>;
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
