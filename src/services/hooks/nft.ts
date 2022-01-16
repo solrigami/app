d56ff9606482d88009e002ae6de3240a8a46f038
Welison Almeida <welison.almeida.923@gmail.com>
2022-01-15 23:34:48 -0300

    Sort wallet nfts data by family and name

--- src/services/hooks/nft.ts
+++ src/services/hooks/nft.ts
@@ -8,5 +8,11 @@ export function useWalletNftList(walletPublicKey: PublicKey | null) {
     async () => await getWalletNftList(walletPublicKey)
   );
 
+  data?.sort(
+    (nft1, nft2) =>
+      nft1.collection?.family.localeCompare(nft2.collection?.family) ||
+      nft1.name.localeCompare(nft2.name)
+  );
+
   return data;
 }
