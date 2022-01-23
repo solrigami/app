aa2f60bab8945880f103e74ced4373971ea364e9
Welison Almeida <welison.almeida.923@gmail.com>
2022-01-23 19:40:55 -0300

    Add title and button  to create NFT page

--- src/pages/CreateNFT/index.tsx
+++ src/pages/CreateNFT/index.tsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
-import Container from "@mui/material/Container";
+import { Grid } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { useWallet } from "@solana/wallet-adapter-react";
+import ButtonWithTooltip from "../../components/ButtonWithTooltip";
+import Title from "../../components/Title";
 
 export default function AllowedProducts() {
   const { publicKey } = useWallet();
 
   return (
-    <Container component="main">
-      <Box m={2}>
-        <Typography variant="h5" component="h3" gutterBottom>
-          {JSON.stringify(publicKey?.toBase58())}
-        </Typography>
-      </Box>
-    </Container>
+    <>
+      <Grid container display="flex" justifyContent="space-between">
+        <Title title="Criar NFT" />
+        <ButtonWithTooltip
+          tooltipText="Conecte-se a sua carteira digital"
+          disabled={publicKey ? false : true}
+          variant="contained"
+          startIcon={<AddIcon />}
+          size="large"
+        >
+          Criar NFT
+        </ButtonWithTooltip>
+      </Grid>
+      <Grid container>
+        <Grid
+          container
+          xs={4}
+          sx={{ backgroundColor: "orange", height: "100px", width: "100%" }}
+        ></Grid>
+        <Grid
+          container
+          xs={8}
+          sx={{ backgroundColor: "blue", height: "100px", width: "100%" }}
+        ></Grid>
+      </Grid>
+    </>
   );
 }
