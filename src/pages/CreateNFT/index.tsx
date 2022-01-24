2e1e84c79734416faafa8ab2e8ac0afb6464bdc7
Welison Almeida <welison.almeida.923@gmail.com>
2022-01-23 22:14:24 -0300

    Add input fields to create NFT page

--- src/pages/CreateNFT/index.tsx
+++ src/pages/CreateNFT/index.tsx
@@ -1,9 +1,66 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import {
+  Box,
+  FormHelperText,
+  Grid,
+  Slider,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { useWallet } from "@solana/wallet-adapter-react";
 import ButtonWithTooltip from "../../components/ButtonWithTooltip";
 import Title from "../../components/Title";
+
+const royaltiesMarks = [
+  {
+    value: 50,
+    label: "50%",
+  },
+];
+
+function getRoyaltiesText(value: number) {
+  return value + "%";
+}
+
+const PrettoSlider = styled(Slider)({
+  height: 8,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-thumb": {
+    height: 24,
+    width: 24,
+    backgroundColor: "#FFFFFF",
+    border: "2px solid currentColor",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+      boxShadow: "inherit",
+    },
+    "&:before": {
+      display: "none",
+    },
+  },
+  "& .MuiSlider-valueLabel": {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: "unset",
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: "50% 50% 50% 0",
+    backgroundColor: "#FB8500",
+    transformOrigin: "bottom left",
+    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+    "&:before": { display: "none" },
+    "&.MuiSlider-valueLabelOpen": {
+      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+    },
+    "& > *": {
+      transform: "rotate(45deg)",
+    },
+  },
+});
 
 export default function AllowedProducts() {
   const { publicKey } = useWallet();
@@ -23,16 +80,152 @@ export default function AllowedProducts() {
         </ButtonWithTooltip>
       </Grid>
       <Grid container>
-        <Grid
-          container
-          xs={4}
-          sx={{ backgroundColor: "orange", height: "100px", width: "100%" }}
-        ></Grid>
-        <Grid
-          container
-          xs={8}
-          sx={{ backgroundColor: "blue", height: "100px", width: "100%" }}
-        ></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}>
+          <Typography
+            color="primary"
+            variant="h5"
+            component="h2"
+            gutterBottom
+            sx={{
+              textTransform: "uppercase",
+              fontWeight: "500",
+              marginBottom: (theme) => theme.spacing(2),
+            }}
+          >
+            Informações básicas
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={8}>
+              <TextField
+                id="nft-name"
+                required
+                label="Nome"
+                variant="outlined"
+                helperText="Nome do colecionável"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                id="nft-symbol"
+                label="Símbolo"
+                variant="outlined"
+                helperText="Símbolo do colecionável (ex.: BAYC)"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            id="nft-description"
+            label="Descrição"
+            variant="outlined"
+            multiline
+            rows={2}
+            helperText="Descrição do colecionável"
+            fullWidth
+            sx={{ marginTop: (theme) => theme.spacing(3) }}
+          />
+          <Typography
+            color="primary"
+            variant="h5"
+            component="h2"
+            gutterBottom
+            sx={{
+              textTransform: "uppercase",
+              fontWeight: "500",
+              marginBottom: (theme) => theme.spacing(2),
+              marginTop: (theme) => theme.spacing(2),
+            }}
+          >
+            Informações avançadas
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={6}>
+              <TextField
+                id="nft-external-url"
+                label="URL externa"
+                variant="outlined"
+                helperText="URL externa extra em que será possível visualizar o ativo"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Box>
+                <PrettoSlider
+                  aria-label="Royalties to NFT author"
+                  defaultValue={0}
+                  step={1}
+                  marks={royaltiesMarks}
+                  max={50}
+                  valueLabelDisplay="on"
+                  getAriaValueText={getRoyaltiesText}
+                  valueLabelFormat={getRoyaltiesText}
+                />
+                <FormHelperText sx={{ marginTop: "-2.0px" }}>
+                  Porcentagem de royalties paga ao criador a cada venda do ativo
+                </FormHelperText>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            columnSpacing={4}
+            sx={{ marginTop: (theme) => theme.spacing(3) }}
+          >
+            <Grid item xs={6}>
+              <TextField
+                id="nft-collection-name"
+                required
+                label="Grupo do colecionável"
+                variant="outlined"
+                helperText="Nome do grupo de uma coleção de ativos correlatos"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="nft-collection-item"
+                label="Nome identificador"
+                variant="outlined"
+                helperText="Nome que identifica unicamente o ativo na coleção"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Typography
+            color="primary"
+            variant="h6"
+            component="h2"
+            sx={{
+              marginBottom: (theme) => theme.spacing(2),
+              marginTop: (theme) => theme.spacing(2),
+            }}
+          >
+            Propriedades
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={6}>
+              <TextField
+                id="nft-property-name"
+                required
+                label="Nome da propriedade"
+                variant="outlined"
+                helperText="Nome da propriedade relacionada ao ativo (ex.: cor)"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="nft-property-value"
+                label="Valor da propriedade"
+                variant="outlined"
+                helperText="Valor da propriedade nomeada (ex.: azul)"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
