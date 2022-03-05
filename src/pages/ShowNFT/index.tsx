f6e3b7fb42f1630f45599c7369bb164fb595e8ca
Welison Almeida <welison.almeida.923@gmail.com>
2022-03-05 20:46:41 -0300

    Add initial structure to display user NFT

--- src/pages/ShowNFT/index.tsx
+++ src/pages/ShowNFT/index.tsx
@@ -1,14 +1,43 @@
 import React from "react";
 import Title from "../../components/Title";
 import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Icon, Typography } from "@mui/material";
+import SolanaLogo from "../../assets/img/solana-logo.svg";
+
 
 export default function ListNFT() {
   const { mint } = useParams<{ mint: string }>();
   return (
     <>
       <Title title="Explorar NFT" />
-      <Typography>{mint}</Typography>
+      <Grid container>
+        <Grid item key="image" xs={6}>
+          <Card>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              sx={{
+                height: '50vh'
+              }}
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button startIcon={<Icon><img alt="Solana logo" src={SolanaLogo} /></Icon>} size="small" variant="outlined">Visualizar imagem</Button>
+              <Button size="small" variant="outlined">Visualizar token</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item key="image" xs={6}></Grid>
+      </Grid>
     </>
   );
 }
