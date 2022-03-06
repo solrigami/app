18384f2d4fa6998eb194a376acba8d347d3a58cd
Welison Almeida <welison.almeida.923@gmail.com>
2022-03-06 16:41:44 -0300

    Add card to display NFT basic informations

--- src/pages/ShowNFT/index.tsx
+++ src/pages/ShowNFT/index.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Title from "../../components/Title";
 import { useParams } from "react-router-dom";
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardMedia,
+  Divider,
   Grid,
   Icon,
   Link,
+  Typography,
 } from "@mui/material";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import SolanaLogo from "../../assets/img/solana-logo.svg";
 import ArweaveLogo from "../../assets/img/arweave-logo.svg";
 
@@ -55,13 +59,12 @@ export default function ListNFT() {
               component="img"
               alt="green iguana"
               sx={{
-                height: "65vh",
-                width: "100%",
-                objectFit: "contain",
+                height: "auto",
+                maxWidth: "100%",
               }}
-              image="https://arweave.net:443/zUbm91h7mvX-thD1J-V0h-4TXRLHtnDkCdiw-7aKQSs"
+              image="https://www.arweave.net/BSe9kvjgwXVkKxjDriSrMWhqDCGnkrr853fZEU_IqFA?ext=png"
             />
-            <CardActions>
+            <CardActions sx={{ padding: 2 }}>
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
                   <ShowNFTButton
@@ -84,7 +87,57 @@ export default function ListNFT() {
           </Card>
         </Grid>
         <Grid item key="data" xs={12} sm={6}>
-          <Card>Just a test</Card>
+          <Card>
+            <Box padding={3}>
+              <Typography
+                color="primary"
+                variant="h4"
+                component="h3"
+                gutterBottom
+                sx={{
+                  fontWeight: 500,
+                }}
+              >
+                Solrigami Bird #03 (SGB)
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: 300, marginBottom: 2 }}
+              >
+                Coleção exclusiva da plataforma Solrigami com a exibição de
+                pássaros através de origamis. Solrigami Bird #3.
+              </Typography>
+              <Link
+                variant="h6"
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://solrigami.com"
+              >
+                https://solrigami.com
+              </Link>
+            </Box>
+            <Divider />
+            <Box display="flex" justifyContent="space-between" padding={3}>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="flex-end"
+                maxWidth="170px"
+                width="100%"
+              >
+                <Typography variant="h5" sx={{fontWeight: 500}}>
+                  Preço:
+                </Typography>
+                <Typography variant="h5">0,23 SOL</Typography>
+              </Box>
+              <Button
+                variant="contained"
+                startIcon={<AccountBalanceWalletIcon />}
+              >
+                Comprar
+              </Button>
+            </Box>
+          </Card>
         </Grid>
       </Grid>
     </>
