03c4e0c3c53a4b9148fa811107c8aecef199a10b
Welison Almeida <welison.almeida.923@gmail.com>
2022-03-20 13:48:55 -0300

    Minor change to env names

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -1,4 +1,4 @@
-const isBackendEnabled = process.env.REACT_APP_USE_BACKEND === "true";
+const isBackendEnabled = process.env.REACT_APP_IS_BACKEND_ENABLED === "true";
 const endpoint = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 export { isBackendEnabled, endpoint };
